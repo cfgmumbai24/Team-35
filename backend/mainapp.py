227188acd0919785680e293830cd17ddd00daf64
mihireshjoshi <mihireshsjoshi@gmail.com>
2024-06-15 15:37:05 +0530

    Backend Folder with dummy code

---
+++ backend/mainapp.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI,File,UploadFile,Form, Request
+from fastapi.responses import FileResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
